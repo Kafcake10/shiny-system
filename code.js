1a539368b9087e93a222b077a539b562c858cfe3
Kafcake10 <89419839+Kafcake10@users.noreply.github.com>
2021-11-16 19:55:57 +0000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,22 @@
+<><html>
+    <head>
+        <title> The text </title>
+    </head>
+    <body>
+
+    </body>
+</html><script>
+        console.log ("My first project console");
+        var first_name="Ranbir";
+        var last_name="Kapoor";
+        var a= 10;
+        var b= 5;
+        var full_name= first_name.concat(last_name);
+        console.log (full_name)
+        var full_name_length=full_name.length;
+        console.log (full_name_length);
+        var add = a + b;
+        console.log (add)
+        console.log (a)
+        console.log (b)
+    </script></>
